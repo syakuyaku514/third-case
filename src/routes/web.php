11cec73ea6a59bcdec0bd46f0a9ea5153af15d7a
shakuyakusystem <30tinjuu8@gmail.com>
2024-11-18 22:24:17 +0900

    お気に入り機能実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\SoldItemController;
+use App\Http\Controllers\FavoriteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,14 +31,17 @@ Route::middleware('auth')->group(function () {
     Route::get('/mypage', [UserController::class, 'mypage'])->name('mypage');
     Route::get('/mypage', [ProfileController::class, 'showMyPage'])->name('mypage');
     
+    // マイページ関係
     Route::get('/mypage/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::post('/mypage/profile', [ProfileController::class, 'store'])->name('profile.store');
     Route::patch('/mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::put('/mypage/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::get('/mypage/profile/create', [ProfileController::class, 'create'])->name('profile.create');
     
+    // ログアウト機能
     Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 
+    // 商品関係
     Route::get('/sell', [ItemController::class, 'create'])->middleware('auth')->name('items.create');
     Route::post('/sell', [ItemController::class, 'store'])->middleware('auth')->name('items.store');
 
@@ -47,6 +51,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/purchase/address/{item_id}', [SoldItemController::class, 'changeAddress'])->name('address.change');
     Route::post('/purchase/address/{item_id}', [SoldItemController::class, 'changeAddress'])->name('address.change');
     Route::put('/purchase/address/{item_id}', [SoldItemController::class, 'updateAddress'])->name('address.update');
+
+    // お気に入り機能
+    Route::get('/item/{id}', [FavoriteController::class, 'show'])->name('item.show');
+    Route::post('/favorite/toggle', [FavoriteController::class, 'toggleFavorite'])->name('favorite.toggle');
 });
 
 
