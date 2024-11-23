7a80ab3ff2d7fb151cbccea2bb64e93395685d68
shakuyakusystem <30tinjuu8@gmail.com>
2024-11-23 12:47:44 +0900

    ホーム、マイページ一覧表表示

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -9,12 +9,22 @@
 <div class="tab-container">
     <!-- タブボタン -->
     <div class="tabs">
-        <button @click="showTab = 'recommended'" :class="{ active: showTab === 'recommended' }">おすすめ</button>
-        <button @click="showTab = 'mylist'" :class="{ active: showTab === 'mylist' }">マイリスト</button>
+        <form method="GET" action="{{ route('home') }}">
+            <input type="hidden" name="tab" value="recommended">
+            <button type="submit" class="tab-button {{ request('tab', 'recommended') === 'recommended' ? 'active' : '' }}">
+                おすすめ
+            </button>
+        </form>
+        <form method="GET" action="{{ route('home') }}">
+            <input type="hidden" name="tab" value="mylist">
+            <button type="submit" class="tab-button {{ request('tab') === 'mylist' ? 'active' : '' }}">
+                マイリスト
+            </button>
+        </form>
     </div>
 
     <!-- 商品リスト表示 -->
-    <div v-if="showTab === 'recommended'">
+    @if(request('tab', 'recommended') === 'recommended')
         <div class="item-list">
             @foreach($items as $item)
                 <div class="item">
@@ -24,11 +34,24 @@
                 </div>
             @endforeach
         </div>
-    </div>
+    @endif
 
     <!-- お気に入りリスト表示 -->
-    <div v-if="showTab === 'mylist'">
-    </div>
+    @if(request('tab') === 'mylist')
+        <div class="item-list">
+            @if($favoriteItems->isEmpty())
+                <p>お気に入り登録した商品がありません。</p>
+            @else
+                @foreach($favoriteItems as $item)
+                    <div class="item">
+                        <a href="{{ url('/item/' . $item->id) }}">
+                            <img src="{{ $item->image ? asset('storage/' . $item->image) : asset('images/default.png') }}" alt="{{ $item->name }}">
+                        </a>
+                    </div>
+                @endforeach
+            @endif
+        </div>
+    @endif
 </div>
 
 @endsection
