3efe0bc3b149c51125f8a45c910d488daa923ad9
shakuyakusystem <30tinjuu8@gmail.com>
2024-12-18 20:36:00 +0900

    バリデーション機能実装

--- src/resources/views/admin/register.blade.php
+++ src/resources/views/admin/register.blade.php
@@ -7,25 +7,36 @@
 </head>
 <body>
     <h1>管理者登録フォーム</h1>
-    <form action="{{ route('admin.register.submit') }}" method="POST">
-        @csrf
-        <div>
-            <label for="name">名前:</label>
-            <input type="text" name="name" id="name" required>
+    <div>
+        @if ($errors->any())
+        <div style="color: red;">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
         </div>
-        <div>
-            <label for="email">メールアドレス:</label>
-            <input type="email" name="email" id="email" required>
-        </div>
-        <div>
-            <label for="password">パスワード:</label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <div>
-            <label for="password_confirmation">パスワード確認:</label>
-            <input type="password" name="password_confirmation" id="password_confirmation" required>
-        </div>
-        <button type="submit">登録</button>
+        @endif
+    </div>
+    <form action="{{ route('admin.register.submit') }}" method="POST" novalidate>
+    @csrf
+    <div>
+        <label for="name">名前:</label>
+        <input type="text" name="name" id="name" value="{{ old('name') }}" required>
+    </div>
+    <div>
+        <label for="email">メールアドレス:</label>
+        <input type="email" name="email" id="email" value="{{ old('email') }}" required>
+    </div>
+    <div>
+        <label for="password">パスワード:</label>
+        <input type="password" name="password" id="password" required>
+    </div>
+    <div>
+        <label for="password_confirmation">パスワード確認:</label>
+        <input type="password" name="password_confirmation" id="password_confirmation" required>
+    </div>
+    <button type="submit">登録</button>
     </form>
 </body>
 </html>
